a5993a2711c501d6c9ec27a490a37f523972ea16
Jekal <0878030025@uscis.dhs.gov>
2021-08-20 15:03:45 -0400

    getting affinity propagation to work

--- affinity_propagation_demo.py
+++ affinity_propagation_demo.py
@@ -1,6 +1,11 @@
+# for data prep, clustering and evaluation
 from sklearn.cluster import AffinityPropagation
 from sklearn import metrics
 import numpy as np
+
+# for plotting
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import cm
 
 def create_data(nobs, vert_scale, horz_scale):
     t = np.linspace(0,2*np.pi,nobs)
@@ -19,15 +24,15 @@ def create_data(nobs, vert_scale, horz_scale):
     
     x_data = g1_x.tolist()+g2_x.tolist()+g3_x.tolist()+g4_x.tolist()
     y_data = g1_y.tolist()+g2_y.tolist()+g3_y.tolist()+g4_y.tolist()
-    return ([list(a) for a in zip(x_data, y_data)], [0 for a in range(len(g1_x))] + [1 for a in range(len(g2_x))] + [2 for a in range(len(g3_x))] + [3 for a in range(len(g4_x))])
+    return (np.array([list(a) for a in zip(x_data, y_data)]), [0 for a in range(len(g1_x))] + [1 for a in range(len(g2_x))] + [2 for a in range(len(g3_x))] + [3 for a in range(len(g4_x))])
 
 if __name__ == '__main__':
     # #############################################################################
     # Generate sample data
-    data, labels_true = create_data(500,3,1)
+    X, labels_true = create_data(500,3,1)
     # #############################################################################
     # Compute Affinity Propagation
-    af = AffinityPropagation(max_iter=500000, verbose=True).fit(data)
+    af = AffinityPropagation(max_iter=500000, convergence_iter=2, random_state=1, verbose=True).fit(X)
     cluster_centers_indices = af.cluster_centers_indices_
     labels = af.labels_
 
@@ -42,29 +47,25 @@ if __name__ == '__main__':
     print("Adjusted Mutual Information: %0.3f"
         % metrics.adjusted_mutual_info_score(labels_true, labels))
     print("Silhouette Coefficient: %0.3f"
-        % metrics.silhouette_score(data, labels, metric='sqeuclidean'))
+        % metrics.silhouette_score(X, labels, metric='sqeuclidean'))
 
     # #############################################################################
     # Plot result
-    import matplotlib.pyplot as plt
-    from matplotlib.pyplot import cm
 
     plt.close('all')
     plt.figure(1)
     plt.clf()
 
-    X = data
-
-    colors = cm.colors.rainbow(np.linspace(0, 1, n_clusters_))
+    colors = cm.rainbow(np.linspace(0, 1, n_clusters_))
     for k, col in zip(range(n_clusters_), colors):
         class_members = labels == k
         cluster_center = X[cluster_centers_indices[k]]
-        plt.plot(X[class_members, 0], X[class_members, 1], col)
+        plt.plot(X[class_members, 0], X[class_members, 1], '.', c=col)
         plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
                 markeredgecolor='k', markersize=14)
         for x in X[class_members]:
-            plt.plot([cluster_center[0], x[0]], [cluster_center[1], x[1]], col)
+            plt.plot([cluster_center[0], x[0]], [cluster_center[1], x[1]], c=col)
 
     plt.title('Estimated number of clusters: %d' % n_clusters_)
-    plt.pdf("affinity-propagation.pdf")
+    plt.savefig("affinity-propagation.pdf")
     plt.show()
