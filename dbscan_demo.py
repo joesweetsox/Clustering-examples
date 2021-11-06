76f07248cbf54dccd7f279428636f1bab7a0c598
Jekal <0878030025@USCIS.DHS.GOV>
2021-11-06 13:44:50 -0400

    adding DBSCAN as well

--- dbscan_demo.py
+++ dbscan_demo.py
@@ -25,13 +25,9 @@ def create_data(nobs, vert_scale, horz_scale):
     y_data = g1_y.tolist()+g2_y.tolist()+g3_y.tolist()+g4_y.tolist()
     return (np.array([list(a) for a in zip(x_data, y_data)]), [0 for a in range(len(g1_x))] + [1 for a in range(len(g2_x))] + [2 for a in range(len(g3_x))] + [3 for a in range(len(g4_x))])
 
-if __name__ == '__main__':
-    # #############################################################################
-    # Generate sample data
-    X, labels_true = create_data(500,3,1)
-    # #############################################################################
+def dbscan_attempt(X, labels_true, eps):
     # Compute DBSCAN
-    db = DBSCAN(eps=0.175, min_samples=5, metric='euclidean', algorithm='auto', n_jobs=-1).fit(X)
+    db = DBSCAN(eps=eps, min_samples=5, metric='euclidean', algorithm='auto', n_jobs=-1).fit(X)
     core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
     core_samples_mask[db.core_sample_indices_] = True
     labels = db.labels_
@@ -40,17 +36,24 @@ if __name__ == '__main__':
     n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
     n_noise_ = list(labels).count(-1)
 
-    print('Estimated number of clusters: %d' % n_clusters_)
-    print('Estimated number of noise points: %d' % n_noise_)
-    print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-    print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-    print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-    print("Adjusted Rand Index: %0.3f"
-        % metrics.adjusted_rand_score(labels_true, labels))
-    print("Adjusted Mutual Information: %0.3f"
-        % metrics.adjusted_mutual_info_score(labels_true, labels))
-    print("Silhouette Coefficient: %0.3f"
-        % metrics.silhouette_score(X, labels))
+    print('#################################################')
+    print(f"eps: {eps}")
+    try:
+        print('Estimated number of clusters: %d' % n_clusters_)
+        print('Estimated number of noise points: %d' % n_noise_)
+        print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
+        print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
+        print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
+        print("Adjusted Rand Index: %0.3f"
+            % metrics.adjusted_rand_score(labels_true, labels))
+        print("Adjusted Mutual Information: %0.3f"
+            % metrics.adjusted_mutual_info_score(labels_true, labels))
+        print("Silhouette Coefficient: %0.3f"
+            % metrics.silhouette_score(X, labels))
+    except:
+        print("error occurred for eps: {eps}")
+    finally:
+        pass
 
     # #############################################################################
     # Plot result
@@ -74,6 +77,16 @@ if __name__ == '__main__':
         plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
                 markeredgecolor='k', markersize=6)
 
-    plt.title('Estimated number of clusters: %d' % n_clusters_)
-    plt.savefig("dbscan.pdf")
-    plt.show()
+    plt.title(f"EPS: {eps:0.3f}, Estimated number of clusters: {n_clusters_}")
+    plt.savefig(f"dbscan_{eps:0.3f}.pdf")
+
+if __name__ == '__main__':
+    # #############################################################################
+    # Generate sample data
+    X, labels_true = create_data(500,3,1)
+    # #############################################################################
+
+    for eps in np.linspace(0.005, 0.2, 40):
+        dbscan_attempt(X, labels_true, eps)
+
+    
